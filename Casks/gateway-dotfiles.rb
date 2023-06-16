aca435d7c834660cbad5e0fe366b59d68384a9c9
Bevan Kay <email@bevankay.me>
2023-06-16 18:03:34 +1000

    gateway-dotfiles 20230616.1

--- Casks/gateway-dotfiles.rb
+++ Casks/gateway-dotfiles.rb
@@ -1,11 +1,13 @@
 cask "gateway-dotfiles" do
-  version "20230616"
+  version "20230616.1"
   sha256 :no_check
 
   url "https://github.com/gatewaymedia/dotfiles.git",
       branch:   "main"
   name "Gateway Dotfiles"
   homepage "https://github.com/gatewaymedia/dotfiles"
+
+  depends_on cask: "cask-fonts/font-sf-mono"
 
   artifact ".hyper.js", target: "~/.hyper.js"
   artifact ".zshrc", target: "~/.zshrc"
