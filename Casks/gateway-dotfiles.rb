d11e8fd7096df8bdabbc8d3d92e0a7c10d503fe1
Bevan Kay <email@bevankay.me>
2023-06-29 09:29:11 +1000

    gateway-dotfiles 20230628.1

--- Casks/gateway-dotfiles.rb
+++ Casks/gateway-dotfiles.rb
@@ -1,5 +1,5 @@
 cask "gateway-dotfiles" do
-  version "20230629"
+  version "20230628.1"
   sha256 :no_check
 
   url "https://github.com/gatewaymedia/dotfiles.git",
@@ -23,6 +23,7 @@ cask "gateway-dotfiles" do
 
     if omz.exist?
       ohai "Oh My Zsh is installed"
+      system "omz", "update"
     else
       ohai "Installing Oh My Zsh"
       system "sh", "-c", "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
