9546988ae5334b505530b6c93f1eb71abd1e572a
Bevan Kay <email@bevankay.me>
2024-10-07 22:27:08 +1100

    yt-dlp-binary 2024.09.27

--- Casks/yt-dlp-binary.rb
+++ Casks/yt-dlp-binary.rb
@@ -1,6 +1,6 @@
 cask "yt-dlp-binary" do
-  version "2024.08.06"
-  sha256 "0075722103617a080c5bdeeddc88ff278fd6d07c5af951b7f333dae8405a6caf"
+  version "2024.09.27"
+  sha256 "e71e8bf6c7fdbfc82e1e637d45b8aae9af49d4575eb19399a364c36c45281c58"
 
   url "https://github.com/yt-dlp/yt-dlp/releases/download/#{version}/yt-dlp_macos"
   name "yt-dlp"
