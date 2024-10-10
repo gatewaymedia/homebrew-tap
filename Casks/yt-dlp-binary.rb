8fb70b437eec875d769cedb888c3ffc99845fa7c
Bevan Kay <email@bevankay.me>
2024-10-10 17:19:06 +1100

    yt-dlp-binary: add dependency

--- Casks/yt-dlp-binary.rb
+++ Casks/yt-dlp-binary.rb
@@ -2,6 +2,13 @@ cask "yt-dlp-binary" do
   version "2024.10.07"
   sha256 "8265ca802d67202b660752198fb0284aa5a115c5c9c518951964c853747a5219"
 
+  on_monterey :or_older do
+    depends_on cask: "bevanjkay/tap/ffmpeg-static"
+  end
+  on_ventura :or_newer do
+    depends_on formula: "ffmpeg"
+  end
+
   url "https://github.com/yt-dlp/yt-dlp/releases/download/#{version}/yt-dlp_macos"
   name "yt-dlp"
   desc "Feature-rich command-line audio/video downloader"
