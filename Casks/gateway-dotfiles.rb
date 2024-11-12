97def6d77d1f719952655b49c13f20592cf4d318
bevanjkay <email@bevankay.me>
2024-11-12 12:43:26 +1100

    gateway-dotfiles 20241112,6066c7882afffdf59336cb77de4c5bd793a73700

--- Casks/gateway-dotfiles.rb
+++ Casks/gateway-dotfiles.rb
@@ -1,5 +1,5 @@
 cask "gateway-dotfiles" do
-  version "20241112,873a16075246e947e9c18166426b90193657da76"
+  version "20241112,6066c7882afffdf59336cb77de4c5bd793a73700"
   sha256 :no_check
 
   url "https://github.com/gatewaymedia/dotfiles.git",
