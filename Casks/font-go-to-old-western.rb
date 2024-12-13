26f52d73f37b636de0e7972d5883a065ea6d2af2
bevanjkay <email@bevankay.me>
2024-12-13 22:45:27 +1100

    font-go-to-old-western 20241213

--- Casks/font-go-to-old-western.rb
+++ Casks/font-go-to-old-western.rb
@@ -1,7 +1,7 @@
 require "#{HOMEBREW_TAP_DIRECTORY}/gatewaymedia/homebrew-tap/cmd/lib/dafont"
 
 cask "font-go-to-old-western" do
-  version "20170921"
+  version "20241213"
   sha256 :no_check
 
   url "https://dl.dafont.com/dl/?f=go_2_old_western"
