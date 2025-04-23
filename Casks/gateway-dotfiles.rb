bc487562590ac876bb3230397ce74335feb19020
bevanjkay <email@bevankay.me>
2025-04-23 15:11:00 +1000

    gateway-dotfiles 20250423,dc4044cbb230a2d4aaae88433427c72ef2f8ee18

--- Casks/gateway-dotfiles.rb
+++ Casks/gateway-dotfiles.rb
@@ -1,5 +1,5 @@
 cask "gateway-dotfiles" do
-  version "20250423,c9f5669333a09206da936e433a754fdd7dd11ab1"
+  version "20250423,dc4044cbb230a2d4aaae88433427c72ef2f8ee18"
   sha256 :no_check
 
   url "https://github.com/gatewaymedia/dotfiles.git",
