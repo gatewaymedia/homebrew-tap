dd4be824c1b59d65affe75c19478219eda045734
bevanjkay <email@bevankay.me>
2026-06-23 12:26:11 +1000

    reside-info-hub 2.0.1

--- Casks/reside-info-hub.rb
+++ Casks/reside-info-hub.rb
@@ -1,6 +1,6 @@
 cask "reside-info-hub" do
-  version "2.0.0"
-  sha256 "fb768d7cbff17e36978eb2b2a6b7948e4b9e22343e79f147f656c508bf665bd3"
+  version "2.0.1"
+  sha256 "1217acad8f92c6bca7f61cbac1d52cfdab812478d7ec34b1ddae7a8849dd8b8e"
 
   url "https://github.com/bevanjkay/pake-builder/releases/download/reside-info-hub-#{version}/Reside.Info.Hub.dmg",
       verified: "github.com/bevanjkay/pake-builder/"
